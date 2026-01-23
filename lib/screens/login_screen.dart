275c3ce8e3deb7d2cfdcf244dce1a8751ba739be
prateek singh <prateeksingh87951@gmail.com>
2026-01-24 00:31:33 +0530

    login screen improved, unit test done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -31,92 +31,101 @@ class _LoginScreenState extends State<LoginScreen> {
               children: [
                 Image.asset(
                   'assets/images/loginpage.jpg',
-                  height: 120,
+                  height: 200,
                 ),
-                const SizedBox(height: 24),
-                Image.asset('assets/images/loginpagevv.jpg', height: 200,),
-                const SizedBox(height: 32),
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                      fillColor: Colors.grey.shade200,
-                      filled: true,
-                    labelText: "User Id",
-                      hintText: "Enter Your UserID",
-                      border: OutlineInputBorder(
-                        borderRadius:BorderRadius.circular(8)
-                      )
-                  )
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  controller: _passwordController,
-                    obscureText: _isPasswordObscured,
+                const SizedBox(height: 25),
+                Image.asset('assets/images/loginpagevv.jpg', height: 35,),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: _usernameController,
                     decoration: InputDecoration(
                         fillColor: Colors.grey.shade200,
                         filled: true,
-                        labelText: "Password",
-                        hintText: "Enter Your Password",
+                      labelText: "User Id",
+                        hintText: "Enter Your UserID",
                         border: OutlineInputBorder(
-                        borderRadius:BorderRadius.circular(8)
+                          borderRadius:BorderRadius.circular(8)
+                        )
+                    )
+                  ),
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: _passwordController,
+                      obscureText: _isPasswordObscured,
+                      decoration: InputDecoration(
+                          fillColor: Colors.grey.shade200,
+                          filled: true,
+                          labelText: "Password",
+                          hintText: "Enter Your Password",
+                          border: OutlineInputBorder(
+                          borderRadius:BorderRadius.circular(8)
+                          ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordObscured ? Icons.visibility_off : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordObscured = !_isPasswordObscured;
+                            });
+                          },
                         ),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordObscured ? Icons.visibility_off : Icons.visibility,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordObscured = !_isPasswordObscured;
-                          });
-                        },
                       ),
-                    ),
+                  ),
                 ),
                 SizedBox(height: 18),
                 SizedBox(
                   width:double.infinity,
                   height: 40,
-                  child: ElevatedButton(
-                      onPressed: () async {
-                        try {
-            
-                          final email = _usernameController.text.trim();
-                          final password = _passwordController.text.trim();
-            
-                          final authResponse = await supabase.auth.signInWithPassword(
-                            email: email,
-                            password: password,
-                          );
-            
-                          if (authResponse.user != null) {
-            
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          try {
+
+                            final email = _usernameController.text.trim();
+                            final password = _passwordController.text.trim();
+
+                            final authResponse = await supabase.auth.signInWithPassword(
+                              email: email,
+                              password: password,
+                            );
+
+                            if (authResponse.user != null) {
+
+                              if (context.mounted) {
+                                Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(builder: (context) => const SiteListScreen()),
+                                );
+                              }
+                            }
+                          } catch (error) {
+
                             if (context.mounted) {
-                              Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(builder: (context) => const SiteListScreen()),
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Error: ${error.toString()}'),
+                                  backgroundColor: Colors.red,
+                                ),
                               );
                             }
                           }
-                        } catch (error) {
-            
-                          if (context.mounted) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Error: ${error.toString()}'),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shadowColor: const Color(0x802196F3),
-                        elevation: 20,
-                        backgroundColor: Colors.blue.shade300,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)
-                        )
-                      ),
-                      child: Text("Login", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),)
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shadowColor: const Color(0x802196F3),
+                          elevation: 20,
+                          backgroundColor: Colors.blue.shade300,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                          ),
+                        ),
+                        child: Text("Login", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),)
+                    ),
                   ),
                 )
             
